ba6b7371a43a3933f5eee5393c1a6d38c981ddbf
Leticia Zeferino <leticiazeferinoo60@gmail.com>
2022-05-20 14:42:31 -0300

    react alura hook

---
+++ react-hook/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DadosPessoais from "./DadosPessoais";
+
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+  return <DadosPessoais />;
+}
+
+export default FormularioCadastro;
